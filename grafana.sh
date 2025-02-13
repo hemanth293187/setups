55b52a122b44b01053588ca026f24c93ac153450
hemanths <hemanthd.work@gmail.com>
2025-02-13 19:16:12 +0530

    Update grafana.sh

--- grafana.sh
+++ grafana.sh
@@ -1,6 +1,6 @@
-sudo apt-get install -y adduser libfontconfig1
-wget https://dl.grafana.com/enterprise/release/grafana-enterprise_9.4.7_amd64.deb
-sudo dpkg -i grafana-enterprise_9.4.7_amd64.deb
+sudo apt-get install -y adduser libfontconfig1 musl
+wget https://dl.grafana.com/enterprise/release/grafana-enterprise_11.5.1_amd64.deb
+sudo dpkg -i grafana-enterprise_11.5.1_amd64.deb
 sudo /bin/systemctl daemon-reload
 sudo /bin/systemctl enable grafana-server
 sudo /bin/systemctl start grafana-server
