a81d50fcae81cf23e1e2f9be462c340a86b14a59
hemanths <hemanthd.work@gmail.com>
2025-05-03 16:15:21 +0530

    Update nexus.sh

--- nexus.sh
+++ nexus.sh
@@ -1,5 +1,21 @@
 #create amazonlinux ec2 with t2.micro and 30 gb of ebs with port 8081 
 
+wget https://download.sonatype.com/nexus/3/nexus-unix-x86-64-3.79.0-09.tar.gz\
+tar -zxvf nexus-unix-x86-64-3.79.0-09.tar.gz
+yum install java-17-amazon-corretto -y
+sudo user add nexus
+chown -R  nexus:nexus  nexus-3.79.0-09
+sudo sh nexus-3.79.0-09/bin/nexus start
+
+
+
+
+
+
+
+
+#create amazonlinux ec2 with t2.micro and 30 gb of ebs with port 8081 
+
 sudo yum update -y
 sudo yum install wget -y
 sudo yum install java-17-amazon-corretto-headless -y
